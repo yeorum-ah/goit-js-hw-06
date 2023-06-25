025d3b0326d53bb1953c4aabead4cc9e00713eb8
Pangpang <vicleeua@gmail.com>
2023-06-25 21:12:23 +0900

    task1

---
+++ js/task-01.js
@@ -0,0 +1,17 @@
+const categoriesList = document.querySelector('ul#categories');
+
+const items = categoriesList.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${items.length}`);
+
+items.forEach((item) => {
+
+  const heading = item.querySelector('h2');
+  
+  const subItems = item.querySelectorAll('li');
+  
+
+
+  console.log(`Category: ${heading.textContent}`);
+  console.log(`Elements: ${subItems.length}`);
+});
